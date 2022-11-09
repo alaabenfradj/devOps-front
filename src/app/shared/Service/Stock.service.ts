ffe126464dd27145e1659b361d1667d61849fce2
AlaaBenFradj <a.benfradj@sapres.de>
2022-11-09 23:41:21 +0100

    front

--- src/app/shared/Service/Stock.service.ts
+++ src/app/shared/Service/Stock.service.ts
@@ -1,26 +1,25 @@
 import { Injectable } from '@angular/core';
 
-import { HttpClient} from '@angular/common/http';
-
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StockService {
-  readonly API_URL = 'http://localhost:8089/SpringMVC/stock';
+  readonly API_URL = 'http://192.168.149.48:8089/SpringMVC/stock';
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   getAllStocks() {
-    return this.httpClient.get(`${this.API_URL}/retrieve-all-stocks`)
+    return this.httpClient.get(`${this.API_URL}/retrieve-all-stocks`);
   }
-  addStock(stock : any) {
-    return this.httpClient.post(`${this.API_URL}/add-stock`, stock)
+  addStock(stock: any) {
+    return this.httpClient.post(`${this.API_URL}/add-stock`, stock);
   }
-  editStock(stock : any){
-    return this.httpClient.put(`${this.API_URL}/modify-stock`, stock)
+  editStock(stock: any) {
+    return this.httpClient.put(`${this.API_URL}/modify-stock`, stock);
   }
-  deleteStock(idStock : any){
-    return  this.httpClient.delete(`${this.API_URL}/remove-stock/${idStock}`)
+  deleteStock(idStock: any) {
+    return this.httpClient.delete(`${this.API_URL}/remove-stock/${idStock}`);
   }
 }
